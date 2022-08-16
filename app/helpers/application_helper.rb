30325bcd270a001eb38783a7ef63443b9cebe6f5
syosyo10337 <syosyo.ds@gmail.com>
2022-08-16 11:01:04 +0900

    Create custmuhelper full_title and Edit view layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  #ページタイトルが設定されてない場合にもRails Tuo-...が返せるようにする。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
+
+
