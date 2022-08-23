9f87108617bba854833e289ebd439096b2d67c7b
syosyo10337 <syosyo.ds@gmail.com>
2022-08-23 20:10:40 +0900

    review section1-5 Finish layput and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      # page_title + " | " + base_title
+      "#{page_title} | #{base_title}"
     end
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,11 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  # test "should get root" do
-  #   get root_url
-  #   assert_response :success
-  #   assert_select "title", "#{@base_title}"
-  # end
+  
 
   test "should get home" do
     get root_path
