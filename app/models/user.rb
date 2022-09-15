4515f64d1e8c542fbf61aad7159a79506a92383f
syosyo10337 <syosyo.ds@gmail.com>
2022-09-15 19:33:22 +0900

    make relationships modeling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy                                
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   #DBでは扱わない仮想的な属性をユーザオブジェクトに付与し、アクセスできるような状態する。
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :email_downcase
@@ -81,6 +90,23 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
     #self.id -> id 
   end
+
+
+  #ユーザをフォローする
+  def follow(other_user)
+    following << other_user
+    #self.followingの省略
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
   private
     def email_downcase
